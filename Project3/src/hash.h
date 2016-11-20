613088b34b953e45f1886f0765b5eefaf768bbd5
Yigit Polat <yigit.polat@ug.bilkent.edu.tr>
2016-11-20 15:54:07 +0300

    noldu şimdi

--- Project3/src/hash.h
+++ Project3/src/hash.h
@@ -15,25 +15,32 @@
 #define MIN_M 10
 #define MAX_M 1000
 
-struct hash_table {
-	// put your table definition here
-	struct node *head;
-	int count;
-};
-//test123
-struct node {
+struct node{
 	int key;
+	int value;
 	struct node *next;
 };
 
+struct hash_table
+{
+	int N;
+	int M;
+	int count;
+	struct node** table;
+};
 typedef struct hash_table HashTable;
 
-int n;
+//data
+pthread_mutex_t* locks;
+int K;
+
+//func
 HashTable *hash_init (int N, int M);
 int hash_insert (HashTable *hp, int k, int v);
 int hash_delete (HashTable *hp, int k);
 int hash_get (HashTable *hp, int k, int *vptr);
 int hash_destroy (HashTable *hp);
+void printLL(struct node* head);
 
 
 //utility functions
