1d3fd6eb5f027c633bccf399140b87079ec8cec8
gulsumgudukbay <gulsum.gudukbay@ug.bilkent.edu.tr>
2016-11-20 15:37:33 +0200

    sa

---
+++ Project3/src/hash.c
@@ -0,0 +1,50 @@
+/*
+ ============================================================================
+ Name        : Project3.c
+ Author      : Gulsum Gudukbay & Dogukan Yigit Polat
+ Version     :
+ Copyright   : 
+ ============================================================================
+ */
+
+
+#include <stdio.h>
+#include <pthread.h>
+#include "hash.h"
+
+// remove printf debug statements at the end
+
+HashTable *hash_init (int N, int M)
+{
+	printf ("hash_init called\n");
+	return (NULL);
+}
+
+int hash_insert (HashTable *hp, int k, int v)
+{
+	printf ("hash_insert called\n");
+	return (0);
+}
+
+int hash_delete (HashTable *hp, int k) {
+
+	printf ("hash_delete called\n");
+	return (0);
+}
+
+int hash_get (HashTable *hp, int k, int *vptr)
+{
+	printf ("hash_get called\n");
+	return (0);
+}
+
+int hash_destroy (HashTable *hp)
+{
+	printf ("hash_destroy called\n");
+	return (0);
+}
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return 0;
+}
---
+++ Project3/src/hash.h
@@ -0,0 +1,40 @@
+/*
+ * hash.h
+ *
+ * Created on: Nov 20, 2016
+ * Author: Gulsum Gudukbay
+ * Student ID: 21401148
+ */
+#ifndef HASH_H
+#define HASH_H
+
+#include <pthread.h>
+
+#define MIN_N 100
+#define MAX_N 1000
+#define MIN_M 10
+#define MAX_M 1000
+
+struct hash_table
+{
+	// put your table definition here
+	struct node *head;
+	int count;
+};
+
+struct node{
+	int key;
+	struct node *next;
+};
+
+typedef struct hash_table HashTable;
+
+int n;
+HashTable *hash_init (int N, int M);
+int hash_insert (HashTable *hp, int k, int v);
+int hash_delete (HashTable *hp, int k);
+int hash_get (HashTable *hp, int k, int *vptr);
+int hash_destroy (HashTable *hp);
+
+
+#endif /* HASH_H */
